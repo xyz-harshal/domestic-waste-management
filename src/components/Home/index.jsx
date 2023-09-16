c7ccf5dd24a4d13cb5fff32aa9986afc597bba27
unknown <harshal011kamble@gmail.com>
2023-09-16 12:26:03 +0530

    initization of project

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,17 @@
+import "./index.css";
+
+let Home = () => {
+    return (
+        <section id="Home">
+            <div className="home">
+                <img src={require("../../img/modi_ji.jpg")} className="modi-img" />
+                <div className="home-main">
+                    <p className="home-main-head">Cleanliness is the key to survive.</p>
+                    <p className="home-main-des" >This is a government based organization whichs helps people to keep their envirnoment clean. Where the people will be together to improve their neighbourhood.</p>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+export default Home;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,44 @@
+import "./index.css";
+import { BsFillPersonFill } from "react-icons/bs";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { MdOutlineCancel } from "react-icons/md";
+import { useState } from "react";
+let Navbar = () => {
+    let [toggle, setToggle] = useState(false);
+    return (
+        <section id="Navbar">
+            <div id="container">
+                <div className="navbar flex flex-row justify-between">
+                    <div className="nav-logo">
+                        <p>Government<br />of Kerala 🌿</p>
+                    </div>
+                    <div className="navbar-items-collection flex flex-row gap-12 items-center justify-between">
+                        <a><p>Campaings</p></a>
+                        <a><p>Education</p></a>
+                        <a><p>Recycle & Donations</p></a>
+                        <a><p>Report Waste</p></a>
+                    </div>
+                    <div className="credentials flex flex-row items-center gap-2 justify-end">
+                        <p>Login/Register</p>
+                        <BsFillPersonFill size={"1.5rem"} />
+                    </div>
+                    <div className="flex flex-row items-center toggle-buttons">
+                        <RxHamburgerMenu size={"1.7rem"} onClick={() => setToggle(!toggle)} className={toggle ? "invincible" : ""} />
+                        <MdOutlineCancel size={"1.7rem"} onClick={() => setToggle(!toggle)} className={toggle ? "" : "invincible"} />
+                    </div>
+                </div>
+                <div className={toggle ? "navbar-toggle-collection flex flex-col justify-start gap-1" : "invincible"}>
+                    <hr />
+                    <a className="flex flex-row"><p>Login/Register</p><BsFillPersonFill size={"1.5rem"} /></a>
+                    <a><p>Campaings</p></a>
+                    <a><p>Education</p></a>
+                    <a><p>Recycle & Donations</p></a>
+                    <a><p>Report Waste</p></a>
+                </div>
+            </div>
+
+        </section>
+
+    )
+}
+export default Navbar;
